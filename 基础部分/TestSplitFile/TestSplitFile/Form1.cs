21ccdc78f852456169595377abef3a1839d5df6f
clong <184402018@qq.com>
2018-02-09 15:51:18 +0800

    分割大型文件；

---
+++ 基础部分/TestSplitFile/TestSplitFile/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestSplitFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInputFile_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInputFile_ButtonClick(object sender, DevExpress.XtraEditors.Controls.ButtonPressedEventArgs e)
+        {
+            var path = SelectPath();
+            if(string.IsNullOrEmpty(path)) return;
+            btnInputFile.Text = path;
+
+
+        }
+
+        private void btnOutputPath_ButtonClick(object sender, DevExpress.XtraEditors.Controls.ButtonPressedEventArgs e)
+        {
+            var path = SelectFolder();
+            if (string.IsNullOrEmpty(path)) return;
+            btnInputFile.Text = path;
+        }
+
+        // 选择文件：
+        private string SelectPath()
+        {
+            string path = string.Empty;
+            var openFileDialog = new OpenFileDialog()
+            {
+                Filter = "Files (*.*)|*.*"//如果需要筛选txt文件（"Files (*.txt)|*.txt"）
+            };
+            var result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                path = openFileDialog.FileName;
+            }
+            return path;
+        }
+        // 选择路径
+        private string SelectFolder()
+        {
+            string path = string.Empty;
+            System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = fbd.SelectedPath;
+            }
+            return path;
+        }
+
+        private void btnSplitFile_Click(object sender, EventArgs e)
+        {
+            var file =btnInputFile.Text;
+            var splitFileFormat = btnOutputPath.Text;
+            var fileInfo = new FileInfo(file);
+            var name = Path.GetFileNameWithoutExtension(file);//这个就是获取文件名的
+            var extensionName = fileInfo.Extension;
+            splitFileFormat = Path.Combine(splitFileFormat, name + "{0}" + extensionName);
+            SplitFileUtil.SplitFile(file, splitFileFormat);
+        }
+
+       
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+      
+    }
+}
---
+++ 基础部分/TestSplitFile/TestSplitFile/SplitFileUtil.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSplitFile
+{
+    public class SplitFileUtil
+    {
+        /// <summary>
+        /// 输入文件名
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="splitFileFormat"></param>
+        public  static void SplitFile(string file,string splitFileFormat)
+        {
+          
+            long splitMinFileSize = unchecked(10 * 1024 * 1024 * 1204);
+            int splitFileSize = 20 * 1024 * 1024;
+
+            FileInfo fileInfo = new FileInfo(file);
+            if (fileInfo.Length > splitMinFileSize)
+            {
+                Console.WriteLine("判定结果：需要分隔文件！");
+            }
+            else
+            {
+                Console.WriteLine("判定结果：不需要分隔文件！");
+                Console.ReadKey();
+                return;
+            }
+
+            int steps = (int)(fileInfo.Length / splitFileSize);
+            using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    int couter = 1;
+                    bool isReadingComplete = false;
+                    while (!isReadingComplete)
+                    {
+                        string filePath = string.Format(splitFileFormat, couter);
+                        Console.WriteLine("开始读取文件【{1}】：{0}", filePath,
+                            DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+
+                        byte[] input = br.ReadBytes(splitFileSize);
+                        using (FileStream writeFs = new FileStream(filePath, FileMode.Create))
+                        {
+                            using (BinaryWriter bw = new BinaryWriter(writeFs))
+                            {
+                                bw.Write(input);
+                            }
+                        }
+
+                        isReadingComplete = (input.Length != splitFileSize);
+                        if (!isReadingComplete)
+                        {
+                            couter += 1;
+                        }
+                        Console.WriteLine("完成读取文件【{1}】：{0}", filePath,
+                            DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                    }
+                }
+            }
+        }
+}
